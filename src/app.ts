d91a80a8e105dbf13d76cb9deed5a97a5848781a
jimcan92 <jimcan051592@gmail.com>
2024-03-19 16:18:30 +0800

    g

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import prisma from "./prisma";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", async (req, res) => {
+  const r = await prisma.user.create({
+    data: { id: "6110389", name: "Jimboy Cantila", email: "jimcan@gmail.com" },
+  });
+  const users = await prisma.user.findMany();
+  console.log(users);
+
+  res.json({ message: "Hello World" });
+});
+
+app.listen(port, () => {
+  return console.log(`http://localhost:${port}`);
+});
